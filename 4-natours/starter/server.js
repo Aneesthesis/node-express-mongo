62c22b18f2ace62e07af682237d1aaf739be288e
Aneesthesis <trulyanees@gmail.com>
2023-05-30 18:53:03 +0530

    mongoose started

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
 dotenv.config({ path: './config.env' });
+
+const dB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(dB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+  });
 
 const app = require('./app');
 
