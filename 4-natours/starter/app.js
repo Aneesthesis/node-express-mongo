7a33f4209c016eb470915daf1706f7e39fba224f
aneesthesis <trulyanees@gmail.com>
2023-06-10 13:50:07 +0530

    data sanitisation

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,14 +12,38 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES
+// 1) GLOBAL MIDDLEWARES
+// set security HTTP headers
+app.use(helmet());
+
+// development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// limit requests from the same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // IN 1 HOUR
+  message: 'Too many requests from this IP, try again in an hour!',
+});
+
+app.use('/api', limiter);
+
+// body parser, reading data from the body into req.body
+// app.use function sits until it is called
+app.use(express.json({ limit: '10kb' }));
+
+// data sanitisation against nosql query injection
+app.use(mongoSanitize());
+
+// data sanitisation against XSS
+app.use(xss());
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
