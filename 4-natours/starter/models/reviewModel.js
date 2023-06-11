f0f7d92b970818a7413dd52cc48236fdcc49211b
aneesthesis <trulyanees@gmail.com>
2023-06-11 17:58:04 +0530

    reviews...

---
+++ 4-natours/starter/models/reviewModel.js
@@ -0,0 +1,39 @@
+const { Mongoose } = require('mongoose');
+
+const reviewSchema = new Mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty!'],
+    },
+    rating: {
+      type: Number,
+      max: 5,
+      min: 1,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour'],
+    },
+
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'A review must have an author'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
