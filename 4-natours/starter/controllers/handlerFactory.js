28d5d80f45b20d8e25fbb5aeeec90a9328187857
aneesthesis <trulyanees@gmail.com>
2023-06-14 16:56:20 +0530

    setting index for faster querying

--- 4-natours/starter/controllers/handlerFactory.js
+++ 4-natours/starter/controllers/handlerFactory.js
@@ -75,6 +75,7 @@ exports.getAll = (Model) =>
       .limitFields()
       .paginate();
 
+    //const doc = await features.query.explain();
     const doc = await features.query;
 
     // Send response
