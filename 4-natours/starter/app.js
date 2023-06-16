4a69d4e0f550cdfe272584e0d5b52a9e8c3604b6
aneesthesis <trulyanees@gmail.com>
2023-06-16 10:33:00 +0530

    setting up pug in express

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,7 +15,14 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) GLOBAL MIDDLEWARES
+
+// serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // set security HTTP headers
 app.use(helmet());
 
@@ -56,9 +64,6 @@ app.use(
   })
 );
 
-// serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -74,6 +79,11 @@ app.use((req, res, next) => {
 //app.delete('/api/v1/tours/:id', deleteTour);
 
 // ROUTES
+
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
